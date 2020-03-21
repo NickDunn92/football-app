77f391e32031895efa6da59da6a14cfc53899e55
Nick Dunn <nickdunn@nick.home>
2020-03-21 12:20:26 +0000

    Added icon and header to homepage

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import { LeagueTable } from '../components';
+import { Header, Icon } from 'semantic-ui-react'
 
-export interface HomepageProps {
-    
-}
+export interface HomepageProps {}
  
 export const Homepage: React.FC<HomepageProps> = () => {
     return (  
-        <>
-            <h1>Futsbol</h1>
+        <div className='header'>
+            <Header as='h2' icon textAlign='center'>
+                <Icon name='soccer' />
+                <Header.Content>Futsbol</Header.Content>
+            </Header>
             <LeagueTable />
-        </>
+        </div>
     );
 }
